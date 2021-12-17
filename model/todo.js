a495b166caa512070aa98b73f1a1b3de6fb29be1
Guru127 <gurucharan.gcn@gmail.com>
2021-12-17 19:40:38 +0530

    schema are created

---
+++ model/todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+ const todoSchema = new mongoose.Schema({
+     email: {
+         type:String,
+         required: true
+     },
+     task:{
+         type: String,
+         required: true
+     }
+     
+ })
+ module.exports = mongoose.todo('todo', todoSchema)
